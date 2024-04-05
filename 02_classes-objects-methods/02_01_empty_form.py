2ac07eb8299f25cfde35a37ac64974c62f190051
jake-bestland <beja0405@gmail.com>
2024-04-05 11:29:01 -0500

    finished 02_01

--- 02_classes-objects-methods/02_01_empty_form.py
+++ 02_classes-objects-methods/02_01_empty_form.py
@@ -10,3 +10,25 @@
 #
 # Model such an application form as a Python class below, and instantiate
 # a few objects from it.
+
+
+class Patient:
+    """Form a patient fills out at doctors office.  Enter as string."""
+    def __init__(self, name, d_o_b, address, symptoms):
+        self.name = name
+        self.d_o_b = d_o_b
+        self.address = address
+        self.symptoms = symptoms
+
+    def __str__(self):
+        return f"{self.name}, {self.d_o_b}, {self.address}, {self.symptoms}"
+    
+    def __repr__(self):
+        return f"Patient(name={self.name}, d_o_b={self.d_o_b}, address={self.address}, symptoms={self.symptoms})"
+    
+
+pat1 = Patient("Jane Doe", "5/10/85", "main street", "upset stomach")
+pat2 = Patient("Joe Smith", "1/5/87", "1st Ave.", "broken finger")
+
+print(pat1.d_o_b)
+print(pat2.symptoms)
