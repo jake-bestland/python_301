bd7d92105702bb796c8b7004a2fec2b22e5887bd
jake-bestland <beja0405@gmail.com>
2024-04-05 13:25:52 -0500

    finished 02_02

--- 02_classes-objects-methods/02_02_planets.py
+++ 02_classes-objects-methods/02_02_planets.py
@@ -3,4 +3,19 @@
 # Use the appropriate dunder method to get informative output with `print()`
 
 class Planet():
-    pass
+    
+    def __init__(self, name, hr_day, day_year, moon):
+        self.name = name
+        self.hr_day = hr_day
+        self.day_year = day_year
+        self.moon = moon
+    
+    def __str__(self):
+        return f"{self.name} takes {self.hr_day} hours to complete one rotation, {self.day_year} days to orbit the sun, and it has {self.moon} moons."
+    
+    def __repr__(self):
+        return f"Planet(name={self.name}, hr_day={self.hr_day}, day_year={self.day_year}, moon={self.moon})"
+
+m = Planet("Mars", 24.6, 687, 2)
+
+print(m)
