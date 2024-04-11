84e8948ef8e4cd2b9a15078d6c8ef0099662ee7a
jake-bestland <beja0405@gmail.com>
2024-04-11 09:36:58 -0500

    finished 03_02

--- 03_inheritance/03_02_inheritance.py
+++ 03_inheritance/03_02_inheritance.py
@@ -9,3 +9,35 @@
 # 5) Practice planning out and flushing out these classes even more.
 #     Take notes in your notebook. What other attributes could a `Movie()` class
 #     contain? What methods? What should the child classes inherit as-is or overwrite?
+
+
+class Movie:
+    def __init__(self, year, title):
+        self.year = year
+        self.title = title
+
+    def __str__(self):
+        return f"{self.year}, {self.title}"
+    
+
+class RomCom(Movie):
+    pass
+
+class ActionMovie(Movie):
+    def __init__(self, year, title, pg=13):
+        super().__init__(year, title)
+        self.pg = pg
+
+    def __str__(self):
+        return f"{super().__str__()}, {self.pg}"
+    
+
+m = ActionMovie(2023, "The Superhero")
+
+print(m)
+
+### could add length, rating, genre/category, actors
+## get reviews
+## add special features
+## find rotten tomato score
+## rank movie by reviews/score
