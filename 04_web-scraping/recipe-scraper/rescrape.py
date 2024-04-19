1ffcb9453b28ae4d523c9e215c9b080ebfa90ff6
jake-bestland <beja0405@gmail.com>
2024-04-19 11:54:04 -0500

    finished rescrape project

--- 04_web-scraping/recipe-scraper/rescrape.py
+++ 04_web-scraping/recipe-scraper/rescrape.py
@@ -8,4 +8,34 @@
 # into the code base, to get some additional practice in working with your
 # custom Python classes.
 
-URL = "https://codingnomads.github.io/recipes"
+# from projects.cooking_soup import Ingredient, Soup
+import requests
+from bs4 import BeautifulSoup
+from pprint import pprint
+
+URL = "https://codingnomads.github.io/recipes/"
+page = requests.get(URL)
+soup = BeautifulSoup(page.text)
+
+link_url = [URL + link["href"] for link in soup.find_all("a")]
+
+### take names of ingredients as input from user
+ingredients = input("Please enter the ingredients you would like to use: ").split(', ')
+
+### fetch recipe link using provided ingredients.  ## all ingredients must be in recipe
+recipes = []
+check_list = []
+for r_link in link_url:
+    r_page = requests.get(r_link)
+    r_soup = BeautifulSoup(r_page.text)
+    rec_ingred = r_soup.find("div", class_="md")
+    for i in ingredients:
+        if i in rec_ingred.text:
+            check_list.append(i)
+    if check_list == ingredients:
+        recipes.append(r_link)
+    else:
+        check_list.clear()
+
+
+pprint(recipes)
