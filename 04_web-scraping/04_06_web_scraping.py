83c578f8cba6ea647cb606a58426501d3023d93b
jake-bestland <beja0405@gmail.com>
2024-04-22 14:26:14 -0500

    finished 04_06

--- 04_web-scraping/04_06_web_scraping.py
+++ 04_web-scraping/04_06_web_scraping.py
@@ -4,3 +4,16 @@
 # BE RESPECTFUL! Don't scrape sites that don't want to be scraped, and
 # limit the amount of calls you make to their page by saving the response
 # to a file, and parsing the content from that file.
+
+import json
+import requests
+from bs4 import BeautifulSoup
+from pprint import pprint
+URL = "https://www.instructables.com/How-to-Play-Phase-10/"
+
+page = requests.get(URL)
+soup = BeautifulSoup(page.text)
+
+article = soup.article
+for section in article.find_all("section", class_="step"):
+    pprint(section.text)
