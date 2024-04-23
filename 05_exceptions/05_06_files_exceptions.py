576c6db6f282e402f1adeebb06b5d4cd6b4b10f9
jake-bestland <beja0405@gmail.com>
2024-04-23 13:46:29 -0500

    finished lab

--- 05_exceptions/05_06_files_exceptions.py
+++ 05_exceptions/05_06_files_exceptions.py
@@ -13,3 +13,32 @@
 #    should NEVER terminate with a Traceback.
 #     a) Which exception can you expect to encounter? Why?
 #     b) How do you catch it to avoid the program from terminating with a traceback?
+
+
+from pathlib import Path
+
+war_peace_path = Path("//Users/jakebestland/Documents/codingnomads/python-301-main/05_exceptions/books/war_and_peace.txt")
+pride_prej_path = Path("/Users/jakebestland/Documents/codingnomads/python-301-main/05_exceptions/books/pride_and_prejudice.txt")
+crime_pun_path = Path("/Users/jakebestland/Documents/codingnomads/python-301-main/05_exceptions/books/crime_and_punishment.txt")
+
+file_list = []
+with war_peace_path.open() as war_file:
+        wp = war_file.read()
+        file_list.append(wp)
+
+with crime_pun_path.open() as crime_file:
+        cp = crime_file.read()
+        cp = ""
+        file_list.append(cp)
+
+with pride_prej_path.open() as pride_file:
+        pp = pride_file.read()
+        file_list.append(pp)
+
+
+try:
+    for file in file_list:
+        print(file[0])
+
+except IndexError:
+      print("This is an empty file and doesn't have any characters")
