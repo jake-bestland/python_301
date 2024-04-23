41604397ff19482fa40c94c836e174916bdc5ea2
jake-bestland <beja0405@gmail.com>
2024-04-23 14:06:07 -0500

    finished lab

--- 05_exceptions/05_07_find_a_prince.py
+++ 05_exceptions/05_07_find_a_prince.py
@@ -3,3 +3,28 @@
 # like you did in the previous exercise.
 # Raise your `PrinceException()` if the first 100 characters of a book
 # contain the string "Prince".
+
+
+from pathlib import Path
+class PrinceException(Exception): pass
+
+war_peace_path = Path("//Users/jakebestland/Documents/codingnomads/python-301-main/05_exceptions/books/war_and_peace.txt")
+pride_prej_path = Path("/Users/jakebestland/Documents/codingnomads/python-301-main/05_exceptions/books/pride_and_prejudice.txt")
+crime_pun_path = Path("/Users/jakebestland/Documents/codingnomads/python-301-main/05_exceptions/books/crime_and_punishment.txt")
+
+
+with war_peace_path.open() as war_file:
+        wp = war_file.read()
+
+with crime_pun_path.open() as crime_file:
+        cp = crime_file.read()
+
+with pride_prej_path.open() as pride_file:
+        pp = pride_file.read()
+
+try:  
+    if "Prince" in wp[0:100]:
+        raise PrinceException
+
+except PrinceException:
+      print("There is a Prince in the first 100 characters!")
