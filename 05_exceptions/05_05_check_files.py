69404445ff8a7c1bb21fe26657cf2330c2de9a33
jake-bestland <beja0405@gmail.com>
2024-04-23 15:02:15 -0500

    finished lab

--- 05_exceptions/05_05_check_files.py
+++ 05_exceptions/05_05_check_files.py
@@ -4,4 +4,27 @@
 # with specific `except` statements, and continue to do the calculation
 # only if neither of them applies.
 
-file_name = 'integers.txt'
+from pathlib import Path
+
+file_path = Path("/Users/jakebestland/Documents/codingnomads/python-301-main/05_exceptions/integers.txt")
+
+try:
+    # file_name = 'integers.txt'
+
+    # with open(file_name, "r") as int_file:
+    #     numbers = int_file.read()
+    with file_path.open() as int_file:
+        numbers = int_file.read()
+        # num_list = [int(n) for n in numbers]
+        num_list = [int(n) for n in numbers.split()]
+    print(num_list)
+
+
+except IOError:
+    print("Cannot read in this file. This is an invalid filename or location.  Add a path to the file in order to correctly read it in.")
+
+except ValueError:
+    print("This is not a valid integer. split the text to get rid of '/n' contained in the string.")
+
+else:
+    print(f"{num_list[0] * 2}")
