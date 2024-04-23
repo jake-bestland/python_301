c8053ec3e38dbfe09eefc42e5fde5706a9eba300
jake-bestland <beja0405@gmail.com>
2024-04-23 15:29:18 -0500

    removed determine winner and war functions, and inserted them into the code itself.

--- projects/war_card_game.py
+++ projects/war_card_game.py
@@ -7,7 +7,7 @@ class Card:
         rank: 1-13
     """
     suit_names = ["Clubs", "Diamonds", "Hearts", "Spades"]
-    rank_names = [None, "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
+    rank_names = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
 
     def __init__(self, suit=0, rank=2):
         self.suit = suit
@@ -35,7 +35,7 @@ class Deck:
     def __init__(self):
         self.cards = []
         for suit in range(4):
-            for rank in range(1, 14):
+            for rank in range(0, 13):
                 card = Card(suit, rank)
                 self.cards.append(card)
 
@@ -47,10 +47,7 @@ class Deck:
     
     def count_card(self):
         """Counts the amount of cards in a deck"""
-        count = 0
-        for card in self.cards:
-            count += 1
-        return count
+        return len(self.cards)
     
     def pop_card(self, i=-1):
         """removes and returns a card from a deck.  
@@ -90,65 +87,6 @@ class Hand(Deck):
     def __init__(self, label=''):
         self.cards = []
         self.label = label
-        
-    def determine_winner(self, p1_card, p2_card):
-        """determines winner based on the rank of the card.
-        
-        p1_card: the card from player 1
-        p2_card: the card from player 2
-        """
-        self.p1_card = p1_card
-        self.p2_card = p2_card
-        if p1_card > p2_card:
-            return f"win"
-        if p1_card < p2_card:
-            return f"lose"
-        else:
-            return f"tie"
-    
-    def war(self, other, p1_card, p2_card, num=3):
-        """A tiebreaker."""
-        print(f"{self.label} goes to war with {other.label}")
-        p1_w_hand = Hand()
-        p2_w_hand = Hand()
-        p1_w_hand.add_card(p1_card)
-        p2_w_hand.add_card(p2_card)
-        if self.count_card() < 3:
-            num = self.count_card()
-        if other.count_card() < 3:
-            num = other.count_card()
-        tie = True
-        while tie == True:
-            self.move_cards(p1_w_hand, num)
-            other.move_cards(p2_w_hand, num)
-            print(f"{self.label} and {other.label} each lay 2 cards face down. P1={p1_w_hand}.\n P2={p2_w_hand}")
-            p1_w_card = p1_w_hand.pop_card()
-            p2_w_card = p2_w_hand.pop_card()
-            print(f"Player 1 draws: {p1_w_card}")
-            print(f"Player 2 draws: {p2_w_card}")
-            w_result = p1_w_hand.determine_winner(p1_w_card, p2_w_card)
-            if w_result == "win":
-                print(f"{self.label} wins!")
-                self.add_card(p1_w_card)
-                self.add_card(p2_w_card)
-                for card in p1_w_hand.cards:
-                    self.add_card(card)
-                for card in p2_w_hand.cards:
-                    self.add_card(card)
-                tie = False
-            elif w_result == "lose":
-                print(f"{other.label} wins!")
-                other.add_card(p1_w_card)
-                other.add_card(p2_w_card)
-                for card in p1_w_hand.cards:
-                    other.add_card(card)
-                for card in p2_w_hand.cards:
-                    other.add_card(card)
-                tie = False
-            else:
-                p1_w_hand.add_card(p1_w_card)
-                p2_w_hand.add_card(p2_w_card)
-                continue
     
 
 deck = Deck()
@@ -176,20 +114,57 @@ while True:
     p2_card = player2.pop_card(0)
     print(f"Player 1 draws: {p1_card}")
     print(f"Player 2 draws: {p2_card}")
-    result = player1.determine_winner(p1_card, p2_card)
-    if result == "win":
+    if p1_card > p2_card:
         print(f"Player 1 wins the hand and adds {p1_card} and {p2_card} to their hand.")
         player1.add_card(p1_card)
         player1.add_card(p2_card)
         continue
-    elif result == "lose":
+    elif p1_card < p2_card:
         print(f"Player 2 wins the hand and adds {p2_card} and {p1_card} to their hand.")
         player2.add_card(p2_card)
         player2.add_card(p1_card)
         continue
     else:
-        print("It's a tie! Both Players must go to war to win the hand!")
-        player1.war(player2, p1_card, p2_card)
-        # print(f"P1 new hand:{player1}\nP2 new hand:{player2}")
+        print(f"It's a tie! {player1.label} goes to war with {player2.label} to see who wins the hand!")
+        num = 3
+        p1_w_hand = Hand()
+        p2_w_hand = Hand()
+        p1_w_hand.add_card(p1_card)
+        p2_w_hand.add_card(p2_card)
+        if player1.count_card() < 3 and player1.count_card() < player2.count_card():
+            num = player1.count_card()
+        elif player2.count_card() < 3:
+            num = player2.count_card()
+        tie = True
+        while tie == True:
+            player1.move_cards(p1_w_hand, num)
+            player2.move_cards(p2_w_hand, num)
+            print(f"{player1.label} and {player2.label} each lay 2 cards face down. P1={p1_w_hand}.\n P2={p2_w_hand}")
+            p1_w_card = p1_w_hand.pop_card()
+            p2_w_card = p2_w_hand.pop_card()
+            print(f"Player 1 draws: {p1_w_card}")
+            print(f"Player 2 draws: {p2_w_card}")
+            if p1_w_card > p2_w_card:
+                print(f"{player1.label} wins!")
+                player1.add_card(p1_w_card)
+                player1.add_card(p2_w_card)
+                for card in p1_w_hand.cards:
+                    player1.add_card(card)
+                for card in p2_w_hand.cards:
+                    player1.add_card(card)
+                tie = False
+            elif p1_w_card < p2_w_card:
+                print(f"{player2.label} wins!")
+                player2.add_card(p1_w_card)
+                player2.add_card(p2_w_card)
+                for card in p1_w_hand.cards:
+                    player2.add_card(card)
+                for card in p2_w_hand.cards:
+                    player2.add_card(card)
+                tie = False
+            else:
+                p1_w_hand.add_card(p1_w_card)
+                p2_w_hand.add_card(p2_w_card)
+                continue
     
 
