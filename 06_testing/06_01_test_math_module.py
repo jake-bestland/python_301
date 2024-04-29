7d5bb003dafba570d6f5e62de9203a33471434fb
jake-bestland <beja0405@gmail.com>
2024-04-29 14:05:54 -0500

    finished lab

--- 06_testing/06_01_test_math_module.py
+++ 06_testing/06_01_test_math_module.py
@@ -1,2 +1,13 @@
 # Write a unittest test suite with at least two methods that test
 # the functionality of the built-in `math` module.
+
+
+import unittest
+import math
+
+class TestMath(unittest.TestCase):
+    def test_floor_rounds_down(self):
+        self.assertEqual(math.floor(3.4), 3)
+
+    def test_ceil_round_up(self):
+        self.assertEqual(math.ceil(5.2), 6)
