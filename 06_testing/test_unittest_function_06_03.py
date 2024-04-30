68ee80c7ef6075d3cd01e49ac7b62ed48fae644d
jake-bestland <beja0405@gmail.com>
2024-04-30 15:32:25 -0500

    changed name of lab. created test for lab

---
+++ 06_testing/test_unittest_function_06_03.py
@@ -0,0 +1,12 @@
+import unittest
+import unittest_function_06_03
+
+class TestUnittestFunction(unittest.TestCase):
+    def test_share_result(self):
+        self.assertEqual(unittest_function_06_03.share(20, 5), "Each person recieves 4.0.")
+
+    def test_share_string_output(self):
+        self.assertIsInstance(unittest_function_06_03.share(20, 5), str)
+
+    def test_share_fail_test(self):
+        self.assertEqual(unittest_function_06_03.share(20, 5), 4)
