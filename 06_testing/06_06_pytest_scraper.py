37e0225d8ff5d56063acfb107357c1424ceb99c6
jake-bestland <beja0405@gmail.com>
2024-05-02 13:59:26 -0500

    finished lab

--- 06_testing/06_06_pytest_scraper.py
+++ 06_testing/06_06_pytest_scraper.py
@@ -1,2 +1,111 @@
 # Install `pytest` in a virtual environment and rewrite the test suite
 # for your web scraper using `pytest` instead of `unittest`.
+
+import pytest
+import requests
+from bs4 import BeautifulSoup
+from pprint import pprint
+
+
+
+BASE_URL = "https://en.wikipedia.org"
+URL = "https://en.wikipedia.org/wiki/Web_scraping"
+
+def get_page_content(url):
+    """Gets the response from a HTTP call to the URL."""
+    page = requests.get(url)
+    return page
+
+def get_html_content(url):
+    """Gets the HTML from a page."""
+    html = get_page_content(url).text
+    return html
+
+def make_soup(html):
+    """Converts an HTML string to a BeautifulSoup object."""
+    soup = BeautifulSoup(html, "html.parser")
+    return soup
+
+def get_main_body(soup):
+    """Gets the main body content of the wiki page"""
+    main_body = soup.find("div", class_="mw-body-content")
+    return main_body
+
+def get_all_links(soup):
+    """Gets all the links from the given a bs4 object"""
+    main_links = soup.find_all("a")
+    return main_links
+
+def get_non_nav_links(list, soup):
+    """Filters out navigation links from a bs4 object of url links.  The 'list' parameter is the destination for the non navigation links and is what is returned."""
+    list = []
+    for link in soup:
+        if '#' not in link.attrs['href']:
+            list.append(link["href"])
+    return list
+
+
+class TestClass:
+    BASE_URL = "https://en.wikipedia.org/"
+    url = f"{BASE_URL}wiki/Web_scraping"
+        
+    def test_get_valid_html_respoinse(self):
+        index_page = get_page_content(self.BASE_URL)
+        page = get_page_content(self.url)
+        assert index_page.status_code == 200
+        assert page.status_code == 200
+
+    def test_get_html_content_returns_html_string(self):
+        index_html = get_html_content(self.BASE_URL)
+        html = get_html_content(self.url)
+        assert "<!DOCTYPE html>" in index_html
+        assert "<!DOCTYPE html>" in html
+
+    def test_make_soup_makes_soup(self):
+        html = get_html_content(self.url)
+        soup = make_soup(html)
+        assert "<class 'bs4.BeautifulSoup'>" == str(type(soup))
+
+    def test_get_main_body_gets_main_body_soup(self):
+        html = get_html_content(self.url)
+        soup = make_soup(html)
+        main_body = get_main_body(soup)
+        assert len(main_body) > 0
+        assert "<class 'bs4.BeautifulSoup'>" == str(type(soup))
+
+    def test_get_all_links_gets_links_from_main_body(self):
+        html = get_html_content(self.url)
+        soup = make_soup(html)
+        assert len(get_all_links(soup)) > 0
+
+    def test_get_non_nav_links_gets_non_nav_links(self):
+        html = get_html_content(self.url)
+        soup = make_soup(html)
+        assert not "#" in soup
+
+
+if __name__ == "__main__":
+    html = get_html_content(URL)
+    soup = make_soup(html)
+
+    non_nav_link = []
+    main_body = get_main_body(soup)
+    main_links = get_all_links(main_body)
+    all_nnl = get_non_nav_links(non_nav_link, main_links)
+
+    # pprint(all_nnl)
+
+
+    wiki2url = BASE_URL + non_nav_link[0]
+
+    wiki2 = get_html_content(wiki2url)
+    w2soup = make_soup(wiki2)
+
+    w2_body = get_main_body(w2soup)
+
+    # with open("wiki_article.txt", "w") as wiki_article:
+    #     wiki_article.write(w2_body)
+
+
+    with open("wiki_article.txt", "r") as wiki_article:
+        wiki2_text = wiki_article.read()
