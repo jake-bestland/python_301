3ee8a9949d9eee97c6caa85bd869df86b57dd887
jake-bestland <beja0405@gmail.com>
2024-05-06 16:30:25 -0500

    finished lab

--- 07_decorators/07_01_quote_wrap.py
+++ 07_decorators/07_01_quote_wrap.py
@@ -1,3 +1,16 @@
 # Write a decorator function that wraps text output into quotes, e.g.:
 # Hello world! ----> "Hello World!"
 # You can use it to create quotes from text output.
+
+def add_quotes(func):
+    def quote(text):
+        initial_result = func(text)
+        new_result = f'"{initial_result}"'
+        return new_result
+    return quote
+
+@add_quotes
+def phrase(text):
+    return text
+
+print(phrase("Hello world!"))
