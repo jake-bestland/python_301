16b51a7a735fe6d34d899efb43f9d12eec5ca314
jake-bestland <beja0405@gmail.com>
2024-05-07 17:38:15 -0500

    finished lab

--- 07_decorators/07_05_decorated_logging.py
+++ 07_decorators/07_05_decorated_logging.py
@@ -1,3 +1,25 @@
 # Create a custom decorator function that records the execution time of
 # the decorated function and prints the time to your console when the function
 # has finished execution.
+
+from datetime import datetime
+
+def decorator_func(initial_func):
+    def wrapper_func():
+        print("the wrapper function picked some...")
+        return initial_func()
+    return wrapper_func
+
+def time_it(func):
+    def time_stamp():
+        print(f"The function started at: {datetime.now()}")
+        func()
+        print(f"The function ended at: {datetime.now()}.")   
+    return time_stamp
+
+@time_it
+@decorator_func
+def prettify():
+    print("flowers for you")
+
+prettify()
