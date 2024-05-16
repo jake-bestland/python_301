8dc6053c19c9b42dfcd2772772c894667390b8e0
jake-bestland <beja0405@gmail.com>
2024-05-16 17:43:01 -0500

    finished labs

--- 07_decorators/07_02_censorship.py
+++ 07_decorators/07_02_censorship.py
@@ -4,3 +4,30 @@
 #    "I bumped my toe! Shoot!"
 # Would, after decorating it with `@censor()`, return:
 #    "I bumped my toe! S****!"
+
+
+
+def censor(func):
+    def censored_words(text):
+        bad_words = {"Shoot":"S****"}
+        sentence = ""
+        word = ""
+        for char in str(func(text)):
+            if word in bad_words.keys():
+                word = bad_words[word]
+            if char != " " and (char.isalpha() or char == "'"):
+                word += char
+                continue
+            elif len(word) != 0:
+                sentence += word + char + ""
+                word = ""
+            else:
+                sentence += char
+        return sentence
+    return censored_words
+
+@censor
+def repeat(text):
+    return text
+
+print(repeat("I bumped my toe! Shoot!"))
--- 07_decorators/07_04_advanced_censorship.py
+++ 07_decorators/07_04_advanced_censorship.py
@@ -1,3 +1,32 @@
 # Build on top of the censorship exercise and change your decorator function
 # so that you can pass the words it should censor when decorating a function, e.g.:
 # `@censor("shoot", "crab")` would censor the words "shoot" and "crab".
+
+def censor_the_words(*args):
+    def censor(func):
+        def censored_words(text):
+            bad_words = {}
+            for arg in args:
+                bad_words[arg] = arg[0] + ("*" * (len(arg) -1))
+            sentence = ""
+            word = ""
+            for char in str(func(text)):
+                if word in bad_words.keys():
+                    word = bad_words[word]
+                if char != " " and (char.isalpha() or char == "'"):
+                    word += char
+                    continue
+                elif len(word) != 0:
+                    sentence += word + char + ""
+                    word = ""
+                else:
+                    sentence += char
+            return sentence
+        return censored_words
+    return censor
+
+@censor_the_words("Shoot", "crab")
+def repeat(text):
+    return text
+
+print(repeat("I bumped my toe! Shoot! let's get some crab."))
