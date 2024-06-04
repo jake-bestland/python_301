4871239f6f8fb407fab31373e83b37da149474f9
jake-bestland <beja0405@gmail.com>
2024-06-04 10:00:36 -0500

    finished labs

--- projects/make-recipe-page/redditapi-get-recipe-pages.py
+++ projects/make-recipe-page/redditapi-get-recipe-pages.py
@@ -2,3 +2,28 @@
 # and re-build the CodingNomads recipe collection website.
 # If you commit this code to GitHub, make sure to keep your API secrets
 # out of version control, for example by adding them as environment variables.
+
+import praw
+from pathlib import Path
+
+
+reddit = praw.Reddit(
+    client_id="*InsertCLIENT_ID*",
+    client_secret="*InsertCLIENT_SECRET*",
+    password="*InsertPASSWORD*",
+    user_agent="ChangeMeClient/0.1 by YourUsername",
+    username="*InsertUSERNAME*",
+)
+
+subreddit = reddit.subreddit("recipes")
+BASE_URL = "https://www.reddit.com"
+recipe_files = ""
+
+for submission in subreddit.top(time_filter="all", limit=25):
+    link = BASE_URL + submission.permalink
+    recipe_files += (f"<li><a href='{link}'>{submission.title}</a></li>")
+
+main_page = f"<h1>Recipe Colletction</h1><p>This page contains some recipes gathered from Reddit posts</p><ul>{recipe_files}</ul>"
+mp = Path().home().joinpath("Desktop").joinpath("recipes").joinpath("main_recipe_page.html")
+mp.write_text(main_page)
+
--- projects/make-recipe-page/redditapi-get-token.py
+++ projects/make-recipe-page/redditapi-get-token.py
@@ -1,2 +1,25 @@
 # Demonstrate how you can log in to the Reddit API to receive content that
 # requires authentication, using only `requests` and your credentials.
+
+
+
+import requests
+import requests.auth
+from pprint import pprint
+
+client_auth = requests.auth.HTTPBasicAuth('*InsertCLIENT_ID*', '*InsertCLIENT_SECRET*')
+post_data = {"grant_type": "password", "username": "*InsertUSERNAME*", "password": "*InsertPASSWORD*"}
+headers = {"User-Agent": "ChangeMeClient/0.1 by YourUsername"}
+response = requests.post("https://www.reddit.com/api/v1/access_token", auth=client_auth, data=post_data, headers=headers)
+response.json()
+
+headers = {"Authorization": "bearer *InsertACCESS_TOKEN*", "User-Agent": "ChangeMeClient/0.1 by YourUsername"}
+response = requests.get("https://oauth.reddit.com/api/v1/me", headers=headers)
+
+response.json()
+
+recipes = requests.get("https://oauth.reddit.com/r/recipes/hot", headers=headers)
+recipes.json()
+
+for post in recipes.json()['data']['children']:
+    pprint(post)
